82dd80541e4f46d887ab5a8e5c560d1becb04b4c
Bri <bhilliker@gmail.com>
2021-09-28 15:59:31 -0500

    setup database connection

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,15 @@
 // import Sequelize constructor
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
 
 // create connection to our database
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
 
 // export
+module.exports = sequelize;
