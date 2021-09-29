43fc304a84d5f3795c1e5cd828256d0c6eb47d09
Bri <bhilliker@gmail.com>
2021-09-28 22:07:31 -0500

    updated post route to include comment

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -5,7 +5,7 @@ const { Post, User, Comment } = require('../../models');
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: ['id', 'post_url', 'title', 'created_at'],
-        order: [['create_at', 'DESC']],
+        order: [['created_at', 'DESC']],
         include: [
             {
                 model: Comment,
@@ -36,23 +36,31 @@ router.get('/:id', (req, res) => {
         },
         attributes: ['id', 'post_url', 'title', 'created_at'],
         include: [
-          {
-            model: User,
-            attributes: ['username']
-          }
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
         ]
-      })
-        .then(dbPostData => {
-          if (!dbPostData) {
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
             res.status(404).json({ message: 'No post found with this id' });
             return;
-          }
-          res.json(dbPostData);
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        });
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 // POST /api/posts
