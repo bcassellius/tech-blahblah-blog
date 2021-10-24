6a096880839c1505ab98036489d124105284f0a2
Bri <bhilliker@gmail.com>
2021-10-24 14:31:10 -0500

    rearranged logic to correct the order and fix the undefined error

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,7 +10,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'Super secret secret',
+    secret: 'whateveriwant',
     cookie: {},
     resave: false,
     saveUninitialized: true,
@@ -23,15 +19,20 @@ const sess = {
     })
 };
 
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(require('./controllers'));
-app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(require('./controllers'));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('🌀 Now listening on localhost!'));
