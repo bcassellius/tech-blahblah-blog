80e9bf4f02a9269f9a7865a0e92857c08466bc65
Bri <bhilliker@gmail.com>
2021-10-25 16:57:55 -0500

    changed get route to render edit page to findByPk

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -44,10 +44,7 @@ router.get('/', withAuth, (req, res) => {
 
     // GET  /dashboard
 router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findOne({
-        where: {
-          id: req.params.id
-        },
+    Post.findByPk(req.params.id, {
         attributes: [
           'id',
           'post_body',
@@ -68,26 +65,23 @@ router.get('/edit/:id', withAuth, (req, res) => {
             attributes: ['username']
           }
         ]
-      })
-        .then(dbPostData => {
-          if (!dbPostData) {
-            res.status(404).json({ message: 'No post found with this id' });
-            return;
-          }
-    
-          // serialize the data
-          const post = dbPostData.get({ plain: true });
-    
-          // pass data to template
-          res.render('edit-post', { 
-            post, 
-            loggedIn: req.session.loggedIn 
-          });
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        });
+    })
+    .then(dbPostData => {
+        if (dbPostData) {
+        // serialize the data
+            const post = dbPostData.get({ plain: true });
+            res.render('edit-post', {
+                post,
+                loggedIn: true
+            });
+        }else {
+            res.status (404).end();
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
     });
+});
 
 module.exports = router;
