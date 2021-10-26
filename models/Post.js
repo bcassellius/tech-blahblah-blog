9000c31971c5a127f29c465c38dc34489f9af137
Bri <bhilliker@gmail.com>
2021-10-25 22:01:38 -0500

    removed url from model

--- models/Post.js
+++ models/Post.js
@@ -17,22 +17,17 @@ Post.init(
             type: DataTypes.STRING,
             allowNull: false
         },
-        
-        post_url:{
-            type: DataTypes.STRING
-        },
-
         post_body: {
             type: DataTypes.STRING,
-            // allowNull: false,
+            allowNull: false,
         },
         user_id: {
             type: DataTypes.INTEGER,
             references: {
                 model: 'user',
                 key: 'id'
-            }
-        }
+            },
+        },
     },
     {
         sequelize,
