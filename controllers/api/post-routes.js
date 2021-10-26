7c2f1c374c3b87b76df473652d04da267d5cedcf
Bri <bhilliker@gmail.com>
2021-10-26 13:57:54 -0500

    minor syntax plus add sequelize, change req.body to req.session, and add post_body

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
+const sequelize = require("../../config/connection");
 const withAuth = require('../../utils/auth');
 
 // GET /api/posts
@@ -10,20 +11,20 @@ router.get('/', (req, res) => {
         include: [
             {
                 model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
                 include: {
                     model: User,
-                    attributes: ['username']
-                }
+                    attributes: ["username"],
+                },
             },
             {
                 model: User,
-                attributes: ['username']
-            }
-        ]
+                attributes: ["username"],
+            },
+        ],
     })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
         console.log(err);
         res.status(500).json(err);
     });
@@ -33,32 +34,32 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     Post.findOne({
         where: {
-          id: req.params.id
+          id: req.params.id,
         },
         attributes: ['id', 'post_body', 'title', 'created_at'],
         include: [
             {
                 model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
                 include: {
                     model: User,
-                    attributes: ['username']
-                }
+                    attributes: ["username"],
+                },
             },
             {
                 model: User,
-                attributes: ['username']
-            }
-        ]
+                attributes: ["username"],
+            },
+        ],
     })
-    .then(dbPostData => {
+    .then((dbPostData) => {
         if (!dbPostData) {
-            res.status(404).json({ message: 'No post found with this id' });
+            res.status(404).json({ message: "No post found with this id" });
             return;
         }
         res.json(dbPostData);
     })
-    .catch(err => {
+    .catch((err) => {
         console.log(err);
         res.status(500).json(err);
     });
@@ -67,16 +68,16 @@ router.get('/:id', (req, res) => {
 // POST /api/posts
 router.post('/', withAuth, (req, res) => {
     // expects {title: "Bri's Blog is Open!", post_url: 'http://maretales-africanadventure.blogspot.com/' user_id: 1}
-        Post.create({
-            title: req.body.title,
-            post_body: req.body.post_body,
-            user_id: req.body.user_id
-        })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    Post.create({
+        title: req.body.title,
+        post_body: req.body.post_body,
+        user_id: req.session.user_id,
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 // PUT /api/posts/:id
@@ -84,13 +85,14 @@ router.put('/:id', (req, res) => {
     // update a post's title
     Post.update(
         {
-            title: req.body.title
+            title: req.body.title,
+            post_body: req.body.post_body
         },
         {
             where: {
                 id: req.params.id
             }
-        }
+        },
     )
     .then(dbPostData => {
         if (!dbPostData) {
