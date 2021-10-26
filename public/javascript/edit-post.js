90c95fcba0eef00d4bde39f5b1b124a577f42d5e
Bri <bhilliker@gmail.com>
2021-10-25 22:04:22 -0500

    added post_body to render text with title

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,13 +2,16 @@ async function editFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value.trim();
+    const post_body = document.getElementById('post-body').value.trim();
+    
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
+    
     const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
-            title
+            title, post_body
         }),
         headers: {
             'Content-Type': 'application/json'
@@ -23,4 +26,3 @@ async function editFormHandler(event) {
 }
   
 document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
-  
